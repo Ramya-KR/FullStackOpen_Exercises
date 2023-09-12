79312a9a10141adad64a1df66127acf1d04dc8a1
Ramya-KR <ramyakr97@gmail.com>
2023-09-12 17:38:36 +0530

    unicafe 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,18 +11,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
-    setGood(good + 1)
-    
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + bad + neutral)
   }
 
   const handleBad = () => {
-    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + updatedBad + neutral)
   }
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + bad + updatedNeutral)
   }
 
   return (
@@ -35,7 +41,10 @@ const App = () => {
       <p>
         good {good}<br/>
         neutral {neutral}<br/>
-        bad {bad}
+        bad {bad}<br/>
+        all {all} <br/>
+        avergae {(good*1-bad*1)/all} <br/>
+        positive {(good/all)*100}%
       </p>
     </div>
   )
