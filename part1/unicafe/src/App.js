3e91b293d24f7b67c0d87290416963479322e9bc
Ramya-KR <ramyakr97@gmail.com>
2023-09-12 22:04:10 +0530

    unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,5 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const StatisticLine = ({ text, value }) => {
   if (text === "positive") {
     return (
@@ -23,10 +17,11 @@ const StatisticLine = ({ text, value }) => {
   )
 }
 
-const Statistics = (props) => {
-  const average = (props.good * 1 - props.bad * 1) / props.all
-  const pos = (props.good / props.all) * 100
-  if (props.all === 0) {
+const Statistics = ({good,bad,neutral}) => {
+  const all = good + bad + neutral
+  const average = (good - bad) / all
+  const pos = (props.good / all) * 100
+  if (all === 0) {
     return (
       <div>
         <h2>statistics</h2>
@@ -38,14 +33,14 @@ const Statistics = (props) => {
     <div>
       <h2>statistics</h2>
       <table>
-      <tbody>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="all" value={props.all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={pos} />
-      </tbody>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={pos} />
+        </tbody>
       </table>
     </div>)
 }
@@ -55,33 +50,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-
-  const handleGood = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setAll(updatedGood + bad + neutral)
-  }
-
-  const handleBad = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setAll(good + updatedBad + neutral)
-  }
-
-  const handleNeutral = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setAll(good + bad + updatedNeutral)
-  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick={handleGood} text='good' />
-      <Button handleClick={handleNeutral} text='neutral' />
-      <Button handleClick={handleBad} text='bad' />
-      <Statistics good={good} bad={bad} neutral={neutral} all={all} />
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(good+1)}>neutral</button>
+      <button onClick={() => setBad(good+1)}>bad</button>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
