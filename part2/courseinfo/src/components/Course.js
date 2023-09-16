c03fcc53b48bd16178baed3a5e1b19f6cfd6ec48
Ramya-KR <ramyakr97@gmail.com>
2023-09-16 11:57:07 +0530

    exercise 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,6 +1,6 @@
 const Header = ({ course }) => {
     return (
-        <h2>{course.name}</h2>
+        <h2 key={course.id}>{course.name}</h2>
     )
 }
 
@@ -10,19 +10,35 @@ const Part = ({ part }) => {
     )
 }
 
+
 const Content = ({ course }) => {
+    const parts = course.parts
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
     return (
-        <div>{course.parts.map(part => <Part key={part.id} part={part} />)}</div>
+        <div>
+            {course.parts.map(part => <Part key={part.id} part={part} />)}
+            <h4>total of {total} exercises</h4>
+        </div>
 
     )
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
         <div>
-            <Header course={course} />
-            <Content course={course} />
+            <h1>Web development curriculum</h1>
+            {courses.map(course => {
+                return (
+                    <div>
+                        <Header course={course} />
+                        <Content course={course} />
+                    </div>)
+            })}
         </div>
+
+
+
+
     )
 }
 
