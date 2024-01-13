db9bae56de00f0b9da6ed456bae3f6999080e800
Ramya-KR <ramyakr97@gmail.com>
2024-01-13 12:10:26 +0530

    exercise 6.9

--- part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -1,9 +1,10 @@
 import { useSelector, useDispatch } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer"
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        return state.filter === '' ? state.anecdotes : state.anecdotes.filter(n => n.content.toLowerCase().includes(state.filter.toLowerCase()))
+    })
     const dispatch = useDispatch()
-
     return (
         <div>
             {anecdotes
